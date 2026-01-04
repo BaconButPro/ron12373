038de16e89a54d939151dfbaa1456007f83958cf
BaconButPro <leminhnhat843@gmail.com>
2026-01-04 07:34:03 +0700

    Update Bypass.user.js

--- Install-Userscript/Bypass.user.js
+++ Install-Userscript/Bypass.user.js
@@ -2,7 +2,7 @@
 // @name         Bypass Key System [Premium]
 // @homepageURL  https://discord.gg/gFNAH7WNZj
 // @namespace    https://tampermonkey.net/
-// @version      2.2.9
+// @version      3.0.0
 // @description  Premium Userscript Helping you Passing The Adlink
 // @author       Bacon But Pro
 // @match        *://linkvertise.com/*/*
